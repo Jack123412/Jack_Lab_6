2c5399021c47e9b0b15942ecad6bfec53bbb4f77
jjennasin <jennasin@ufl.edu>
2023-10-27 17:41:43 -0400

    added decode function

--- encoder.py
+++ encoder.py
@@ -21,6 +21,16 @@ def encode():  # Encode Function
 
     return result
 
+def decode(encoded_password): # subtracts 3 to each digit
+    decoded = ""  # empty string
+    for i in encoded_password:
+        i = int(i)
+        if i >= 3:  # if the number won't become negative
+            i -= 3  # subtract 3
+        elif i < 3:  # if the number becomes negative, the original number was greater than 10
+            i += 7  # subtracts the original >10 number by 3
+        decoded += str(i)
+    return decoded
 
 __name__ = '__Main__'
 
@@ -40,9 +50,9 @@ if __name__ == '__Main__':
             encode()
             print('Your password has been encoded and stored!\n')
 
-        '''if choice == '2':
+        if choice == '2':
             print(f'The encoded password is {encoded_password}, and the '
-                  f'original password is {decode()}.\n')'''
+                  f'original password is {decode()}.\n')
 
         if choice == '3':
             break
